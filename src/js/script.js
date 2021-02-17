9220a719b651a499f6c777c3961778e1057bec5a
Dima <warreaver123@gmail.com>
2021-02-17 13:29:20 +0200

    carousel is ready

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$('.carousel__inner').slick({
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: `<button type="button" class="slick-prev"><img src="icon/chevron_left_solid_980.png"></img></button>`,
+    nextArrow: '<button type="button" class="slick-next"><img src="icon/chevron_right_solid_982.png"></button>',
+    
+  });
