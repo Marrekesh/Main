149622e3945891dfe5c3bf6f68d92d71061eb2a9
Dima <warreaver123@gmail.com>
2021-02-23 16:50:47 +0200

    catalog part 2

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,9 @@ $('.carousel__inner').slick({
     nextArrow: '<button type="button" class="slick-next"><img src="icon/chevron_right_solid_982.png"></button>',
     
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__items').removeClass('catalog__items_active').eq($(this).index()).addClass('catalog__items_active');
+});
