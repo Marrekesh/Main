b50d9824bccaa3d6ff0317b4b0b8417ed808ffd0
Dima <warreaver123@gmail.com>
2021-03-01 12:24:35 +0200

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -92,10 +92,7 @@ $(document).ready(function(){
       } else {
         $(`.pageup`).fadeOut();
       }
-      // $("a[href^='#']").click(function(){
-      //   var _href = $(this).attr("href");
-      //   $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-      //   return false;
-      // });
     });
+
+    new WOW().init();
 });
